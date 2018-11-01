9c73a1d0bd11c7fce745605cadea541083009b7d
Saskia Imani <imani.saskia@gmail.com>
2018-11-01 10:01:55 +0700

    Revised .h to match .c

--- waitingcust.h
+++ waitingcust.h
@@ -3,32 +3,30 @@
 
 #include "library.h"
 
-/* ********* Prototype ********* */
-boolean IsEmpty (Queue Q);
-/* Mengirim true jika Q kosong. */
-boolean IsFull (Queue Q);
-/* Mengirim true jika tabel penampung elemen Q sudah penuh */
-/* yaitu mengandung elemen sebanyak MaxEl */
-int NBElmt (Queue Q);
-/* Mengirimkan banyaknya elemen queue. Mengirimkan 0 jika Q kosong. */
+/*---------- CUSTOMER ----------*/
+// Konstruktor Customer
+void NewCustomer (Customer * C);    // Menciptakan customer baru
 
-/* *** Kreator *** */
-void CreateEmpty (Queue * Q, int Max);
-/* I.S. sembarang */
-/* F.S. Sebuah Q kosong terbentuk dan salah satu kondisi sbb: */
-/* Jika alokasi berhasil, Tabel memori dialokasi berukuran Max+1 */
-/* atau : jika alokasi gagal, Q kosong dg MaxEl=0 */
-/* Proses : Melakukan alokasi, membuat sebuah Q kosong */
 
-/* *** Primitif Add/Delete *** */
-void Add (Queue * Q, Customer C);
-/* Proses: Menambahkan X pada Q dengan aturan FIFO */
-/* I.S. Q mungkin kosong, tabel penampung elemen Q TIDAK penuh */
-/* F.S. X menjadi TAIL yang baru, TAIL "maju" dengan mekanisme circular buffer */
-void Del (Queue * Q, Customer * C);
-/* Proses: Menghapus X pada Q dengan aturan FIFO */
-/* I.S. Q tidak mungkin kosong */
-/* F.S. X = nilai elemen HEAD pd I.S., HEAD "maju" dengan mekanisme circular buffer; 
-        Q mungkin kosong */
+/*---------- QUEUE ----------*/
+// Uji Queue
+boolean IsEmpty (Queue Q);  // Mengembalikan true jika Q kosong (Head = Tail = 0)
+boolean IsFull (Queue Q);   // Mengembalikan true jika NBElmt = MaxEl
+
+// Jumlah elemen Queue
+int NBElmt (Queue Q);       // Mengembalikan Tail - Head + 1
+
+// Konstruktor Queue
+void CreateEmpty (Queue * Q, int Max);  // Membuat Q kosong dengan MaxEl = Max
+
+// Tambah/Hapus Elemen Queue
+void Add (Queue * Q, Customer C);           // Menambah elemen baru ke belakang Q
+void SpecialAdd (Queue * Q, Customer C);    // Menambah elemen baru ke depan Q
+void Del (Queue * Q, Customer * C);         // Menghapus elemen paling depan Q
+void DelX (Queue * Q, int X, Customer * C); // Menghapus elemen ke-X
+
+// Menentukan apa yang terjadi pada Queue setelah 1 tick
+void LessPatient (Queue * Q);   // Mengurangi Patience semua Customer; menghapus Customer yang Patience = 0
+void UpdateQueue (Queue * Q);   // LessPatient; menentukan adanya Customer baru
 
 #endif
