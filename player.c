b4a29f4de1cc2ce07621dbede159fc69475892b7
nandorusrin <nando_rusrin@yahoo.com>
2018-11-26 05:54:19 +0700

    player

---
+++ player.c
@@ -0,0 +1,14 @@
+#include "player.h"
+
+void CreatePlayer(Player *P) {
+/*
+Player P;
+    P.Life = 0;
+    P.Money = 0;
+    P.Name = "Dummy";
+    P.Position.i = 1;
+    P.Position.j = 1;
+    P.Room = 1;
+    P.Time = 0;   
+*/
+}
